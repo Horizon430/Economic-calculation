4c8d13cd0fba6d6de0466f588300722d4278014b
您的GitHub用户名 <GitHub>
2026-04-20 02:27:43 +0800

    配置 GitHub Actions 自动部署

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Economic-calculation/', // 加上这一行，注意前后都有斜杠
   test: {
     environment: 'jsdom',
     globals: true,
